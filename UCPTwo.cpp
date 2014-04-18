514655bd8cc30c395d862b4163ad85c506667251
Josh <639909@gmail.com>
2014-04-19 00:34:54 +0100

    Full propagation
    Program takes file as user input
    Full propagation now takes place
    Program counts clauses and variables after propagation and updates
    problem line
    Original comments printed to file
    New comments stating when propagation took place printed to file
    If no file is entered for input a message appears stating a file is
    needed

--- UCPTwo.cpp
+++ UCPTwo.cpp
@@ -4,14 +4,19 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <ctime>
 
-int main(int argc, char *argv[]) {
+int main(const int argc, char *argv[]) {
   //input
+  if (argc!=2){
+	std::cout << "Usage: ./UCP <Input File>";
+	return 0;
+  }
   std::set<int> clause;
   std::vector<std::set<int> > clauseVector;
-  std::vector<std::string> commentsVector, problemVector;
-  std::ifstream inFile("in.cnf");
-  std::string currentClause;
+  std::vector<std::string> commentsVector;
+  std::ifstream inFile (argv[1]);
+  std::string currentClause, problem;
   while (std::getline(inFile, currentClause)){
 	std::istringstream iss(currentClause);
 	std::string c("c");
@@ -19,9 +24,9 @@ int main(int argc, char *argv[]) {
 	  commentsVector.push_back(currentClause);
 	std::string p("p");
 	if(currentClause.compare(0, p.length(), p)==0)
-      problemVector.push_back(currentClause);
+      problem = currentClause;
 	int variable;
-	for (std::istringstream iss(currentClause); iss >> variable;) 
+	for (std::istringstream iss(currentClause); iss >> variable;)
 	  if (variable != 0)
 	    clause.insert(variable);
       if (variable == 0){
@@ -29,6 +34,14 @@ int main(int argc, char *argv[]) {
         clause.clear();
      }
   }
+  int variableCount, clauseCount;
+  problem.erase (0,6);
+  std::stringstream str(problem);
+  str >> variableCount >> clauseCount; 
+  std::cout << "Variables: " << variableCount << " Clauses: " << clauseCount;
+
+  int commentCount = commentsVector.size() +1;
+  clauseVector.erase(clauseVector.begin(),clauseVector.begin() + commentCount);
   //processing
   std::set<int> tempSet;
   int propagator = 0;
@@ -39,15 +52,32 @@ int main(int argc, char *argv[]) {
       for(const int i : clause)
 	    propagator = i;
 	if(propagator!= 0){
-      auto iter = std::remove_if( clauseVector.begin(), clauseVector.end(),[propagator] ( const std::set<int>& s ){ 
-	    return s.find(propagator) != s.end() ; } ) ;
+      auto iter = std::remove_if( clauseVector.begin(), clauseVector.end(),[propagator] ( const std::set<int>& i ){ 
+	    return i.find(propagator) != i.end() ; } ) ;
       clauseVector.erase( iter, clauseVector.end() ) ;
+    int secondPropagator;
+    if (propagator >0)
+      secondPropagator = -abs(propagator);
+    else if (propagator < 0)
+      secondPropagator = abs(propagator);
+    std::for_each(clauseVector.begin(), clauseVector.end(), [&](std::set<int>& s){s.erase(secondPropagator);});
     }
     tempSet.clear();
     propagator = 0;
   }
   //output
-  std::ofstream outFile("in.cnf");
+  std::set<int> clauseCountV;
+  time_t now = time(0);
+  char* dt = ctime(&now);
+  std::ofstream outFile("out.cnf");
+  for (const auto &comments : commentsVector)
+      outFile << comments << "\n";
+  outFile << "c This file was propagated on the " << dt;
+  for(std::set<int> const &mySet : clauseVector)
+    for(const int i : mySet)
+	  clauseCountV.insert(i);
+  outFile << "p cnf " << clauseCountV.size() << " " << clauseVector.size() <<  "\n";
+
   for(std::set<int> const &mySet : clauseVector){
     for(const int i : mySet){
       outFile << i << " ";
