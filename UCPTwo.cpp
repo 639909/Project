762401290bf792148c5d9c55bc0a203d47f0601c
Josh <639909@gmail.com>
2014-04-18 14:00:17 +0100

    Program now takes as input
    Comments are added to a comments vector for later printing
    Problem line is added to a problem vector for later printing
    Program uses line break(0) to differentiate between clauses

--- UCPTwo.cpp
+++ UCPTwo.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 int main(int argc, char *argv[]) {
-    std::set<int> clause;
-	std::vector<std::set<int> > clauseVector;
-
-	std::cout<<"Clauses added "<<(clauseVector.size());
-	  
-	std::set<int> tempSet;
-    int propagator = 0;
-    for(auto clause : clauseVector){
+  std::set<int> clause;
+  std::vector<std::set<int> > clauseVector;
+  std::vector<std::string> commentsVector, problemVector;
+  std::ifstream inFile("input.txt");
+  std::string currentClause;
+  while (std::getline(inFile, currentClause)){
+	std::istringstream iss(currentClause);
+	std::string c("c");
+	if(currentClause.compare(0, c.length(), c)==0)
+	  commentsVector.push_back(currentClause);
+	std::string p("p");
+	if(currentClause.compare(0, p.length(), p)==0)
+      problemVector.push_back(currentClause);
+	int variable;
+	for (std::istringstream iss(currentClause); iss >> variable;) 
+	  if (variable != 0)
+	    clause.insert(variable);
+      if (variable == 0){
+	    clause.insert(variable);
+	    clauseVector.push_back(clause);
+        clause.clear();
+     }
+  }
+  std::set<int> tempSet;
+  int propagator = 0;
+  for(auto clause : clauseVector){
+    for(const int i : clause)
+	  tempSet.insert(i);
+    if(tempSet.size() == 1)
       for(const int i : clause)
-	    tempSet.insert(i);
-	  if(tempSet.size() == 1)
-	    for(const int i : clause)
-	      propagator = i;
-		if(propagator!= 0)
-	      std::cout << " Propagator = " <<propagator << "\n";
-	    tempSet.clear();
-	    propagator = 0;
-	}
+	    propagator = i;
+	if(propagator!= 0)
+    tempSet.clear();
+    propagator = 0;
+  }
 }
